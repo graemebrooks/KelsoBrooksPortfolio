7c3dcc4ad34bd2a9d3970c1a547367e2687e96a7
Kelso <graemebrooks@gmail.com>
2020-06-08 16:23:04 -0500

    Updates

---
+++ precache-manifest.c02de58c402a14c07475789ebf332b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1437b55ad7ba8ae3f87f4fe2f253220e",
+    "url": "/KelsoBrooksPortfolio/index.html"
+  },
+  {
+    "revision": "f057d3977ebbce47d89e",
+    "url": "/KelsoBrooksPortfolio/static/css/main.87a953ca.chunk.css"
+  },
+  {
+    "revision": "969bddc4394b1e92437b",
+    "url": "/KelsoBrooksPortfolio/static/js/2.e80cdf44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KelsoBrooksPortfolio/static/js/2.e80cdf44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f057d3977ebbce47d89e",
+    "url": "/KelsoBrooksPortfolio/static/js/main.97dbb583.chunk.js"
+  },
+  {
+    "revision": "5d6d0c5750694d57fb1a",
+    "url": "/KelsoBrooksPortfolio/static/js/runtime-main.39c6c3bc.js"
+  }
+]);
